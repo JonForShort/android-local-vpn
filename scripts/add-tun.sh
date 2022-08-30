e333d2ef02062a55b5f4ba43175d038272eeae03
dev <6559571+JonForShort@users.noreply.github.com>
2022-08-30 16:33:33 -0700

    Scripts can now be run from any directory.

--- scripts/add-tun.sh
+++ scripts/add-tun.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -x
 
-# create tun device and change state to 'up'.
-sudo ip tuntap add name tun0 mode tun user $USER
-sudo ip link set tun0 up
+# current directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# save routing table before modifying it.
-sudo iptables-save > iptables.bak
+main() {
+    pushd ${SCRIPT_DIR}
+    
+    # create tun device and change state to 'up'.
+    sudo ip tuntap add name tun0 mode tun user $USER
+    sudo ip link set tun0 up
+    
+    # save routing table before modifying it.
+    sudo iptables-save > iptables.bak
+    
+    # route everything through tun device.
+    sudo ip route add 128.0.0.0/1 dev tun0
+    sudo ip route add 0.0.0.0/1 dev tun0
 
-# route everything through tun device.
-sudo ip route add 128.0.0.0/1 dev tun0
-sudo ip route add 0.0.0.0/1 dev tun0
+    popd
+}
+
+main
--- scripts/remove-tun.sh
+++ scripts/remove-tun.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -x
 
-# delete tun device.
-sudo ip link delete tun0
+# current directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# restore ip tables.
-sudo iptables-restore < iptables.bak
+main() {
+    pushd ${SCRIPT_DIR}
+    
+    # delete tun device.
+    sudo ip link delete tun0
+    
+    # restore ip tables.
+    sudo iptables-restore < iptables.bak
+    
+    popd
+}
+
+main
