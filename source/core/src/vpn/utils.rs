0a45bd26d0e57f341de559485e2cc160063d7cd6
dev <6559571+JonForShort@users.noreply.github.com>
2023-01-01 14:29:38 -0800

    Logging UDP packets

--- source/core/src/vpn/utils.rs
+++ source/core/src/vpn/utils.rs
@@ -23,15 +23,15 @@
 //
 // For more information, please refer to <https://unlicense.org>
 
-use smoltcp::wire::{IpProtocol, Ipv4Packet, TcpPacket};
+use smoltcp::wire::{IpProtocol, Ipv4Packet, TcpPacket, UdpPacket};
 
 extern crate log;
 
 pub fn log_packet(message: &str, bytes: &Vec<u8>) {
     let result = Ipv4Packet::new_checked(&bytes);
     match result {
-        Ok(ip_packet) => {
-            if ip_packet.protocol() == IpProtocol::Tcp {
+        Ok(ip_packet) => match ip_packet.protocol() {
+            IpProtocol::Tcp => {
                 let tcp_bytes = ip_packet.payload();
                 let tcp_packet = TcpPacket::new_checked(tcp_bytes).unwrap();
                 log::debug!(
@@ -42,10 +42,23 @@ pub fn log_packet(message: &str, bytes: &Vec<u8>) {
                     tcp_bytes.len(),
                     ip_packet
                 );
-            } else {
+            }
+            IpProtocol::Udp => {
+                let udp_bytes = ip_packet.payload();
+                let udp_packet = UdpPacket::new_checked(udp_bytes).unwrap();
+                log::debug!(
+                    "[{:?}] len={:?} udp=[{}] udp_len={:?} ip=[{}]",
+                    message,
+                    bytes.len(),
+                    udp_packet,
+                    udp_bytes.len(),
+                    ip_packet
+                );
+            }
+            _ => {
                 log::debug!("[{:?}] len={:?} ip=[{}]", message, bytes.len(), ip_packet);
             }
-        }
+        },
         Err(error) => {
             log::error!("[{:?}] failed to log packet, error={:?}", message, error);
         }
